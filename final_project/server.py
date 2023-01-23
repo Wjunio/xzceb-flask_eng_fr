faec18a54bd1b449201063b2497d45d5d9aeffde
Welington Junio Rezende Mateus <wj.mateus12@hotmail.com>
2023-01-22 21:39:33 -0300

    Update server.py
    implementando tradução e chamada do template

--- final_project/server.py
+++ final_project/server.py
@@ -6,13 +6,20 @@ app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')    
-    return translator.english_to_french(textToTranslate)
+    textToTranslate = request.args.get('textToTranslate')
+    print(textToTranslate)
+    textoTranslate = translator.english_to_french(textToTranslate)
+    print(textoTranslate)
+    return textoTranslate
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     return translator.french_to_english(textToTranslate)
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
